b039be9c8cb7bc76af02aeff806c9f6bf6c75e62
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-02-11 14:22:51 -0600

    Set up user creation and add signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,23 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        #user.valid?
+        if @user.save
+            redirect_to root_path, notice: "You done it, #{@user.lastname}"
+        else
+            render :signup#, notice: "you screwed up"
+        end
+    end
+
+    def signup
+        @user = User.new
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :firstname, :lastname, :email, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    # use bcrypt gem to ensure strong password
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, presence: true
 end
