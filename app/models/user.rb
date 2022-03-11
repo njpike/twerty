5b3e39889bd4218fd1ecc55279805ebaa13eb6ef
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-03-11 13:46:24 -0600

    Make password attribute only validate in create context

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
     validates :email, uniqueness: true
-    validates :password, presence: true
+    validates :password, presence: true, on: :create
 end
