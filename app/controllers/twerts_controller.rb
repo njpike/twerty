2519cea86acf5217ad48e9ed8978112e8c7e5b75
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-04-01 13:38:53 -0500

    Able to create twerts

--- app/controllers/twerts_controller.rb
+++ app/controllers/twerts_controller.rb
@@ -1,7 +1,18 @@
 class TwertsController < ApplicationController
-    def index
-    end
+	def index
+	end
 
-    def create
-    end
+	def create
+		twert = Twert.new(twert_params)
+
+		if twert.save
+			redirect_to root_path, notice: "Thanks for your thoughts, I'll toss them out into the wide open ocean polluted with other meaningless thought bottles"
+		else
+			render :index, notice: "Try again, friend-o"
+		end
+	end
+
+	def twert_params
+		params.require(:twert).permit(:content).merge(user: current_user)
+	end
 end
