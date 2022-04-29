8c7fc71fb56e2787fb1c5e7cae1a6a12f2242c49
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-04-29 13:32:48 -0500

    ocd tab to space consistency fix for old migration file

--- db/migrate/20220325183320_create_twerts.rb
+++ db/migrate/20220325183320_create_twerts.rb
@@ -1,11 +1,11 @@
 class CreateTwerts < ActiveRecord::Migration[6.1]
-	def change
-		create_table :twerts do |t|
-			t.references :user, foreign_key: true, null: false
-			t.string :content, null: false, limit: 140
-			t.references :parent_twert, foreign_key: { to_table: :twerts }
+  def change
+    create_table :twerts do |t|
+      t.references :user, foreign_key: true, null: false
+      t.string :content, null: false, limit: 140
+      t.references :parent_twert, foreign_key: { to_table: :twerts }
 
-			t.timestamps
-		end
-	end
+      t.timestamps
+    end
+  end
 end
