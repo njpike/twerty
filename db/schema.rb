35857fdeda490a39ef9bf2331381c3a4158bfc9f
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-04-29 13:35:34 -0500

    Create likeable db table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_183320) do
+ActiveRecord::Schema.define(version: 2022_04_29_181729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "likeable_type", null: false
+    t.bigint "likeable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "twerts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -35,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_03_25_183320) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "users"
   add_foreign_key "twerts", "twerts", column: "parent_twert_id"
   add_foreign_key "twerts", "users"
 end
