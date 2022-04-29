e85fba388aa58f268d11bc64f36b4e91637199d6
Nathan Pike <10805795+njpike@users.noreply.github.com>
2022-04-29 13:53:10 -0500

    Create Like model and polymorphic associations with User and Twert

--- app/models/twert.rb
+++ app/models/twert.rb
@@ -1,7 +1,9 @@
 class Twert < ApplicationRecord
 	belongs_to :user
-	has_many :replies, class_name: "Twert", foreign_key: "parent_twert_id"
 	belongs_to :parent_twert, class_name: "Twert", optional: true
+
+	has_many :replies, class_name: "Twert", foreign_key: "parent_twert_id"
+	has_many :likes, as: :likeable
 
 	validates :user, :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-    # use bcrypt gem to ensure strong password
-    has_secure_password
+	# use bcrypt gem to ensure strong password
+	has_secure_password
 
-    validates :username, presence: true, uniqueness: true
-    validates :email, uniqueness: true
-    validates :password, presence: true, on: :create
+	has_many :likes
+	has_many :user_likes, as: :likeable
+
+	validates :username, presence: true, uniqueness: true
+	validates :email, uniqueness: true
+	validates :password, presence: true, on: :create
 end
